abaa867a9626b47dcfafa0a3aff3a64c97a2ff45
Ericson Willians Rocha Pires. <ericsonwrp@gmail.com>
2014-08-18 03:18:09 -0300

    Swaped tabs for spaces.

--- Wordgen.py
+++ Wordgen.py
@@ -8,21 +8,21 @@ import Pythorology
 
 class WordGen:
 
-	def __init__(self, v, c):
-		w = open("w.txt", "r")
-		self.words = [x.replace('\n', '') for x in w.readlines()]
-		self.v = v # Vowel.
-		self.c = c # Consonant.
-		self.decoded = [(x, pythorology.Vcipher(x).v.to_one(), pythorology.Vcipher(x).c.to_one()) for x in self.words]
-	
-	def _filter(self):
-	
-		return [x for x in self.decoded if x[1] == self.v and x[2] == self.c]
-		
+    def __init__(self, v, c):
+        w = open("w.txt", "r")
+        self.words = [x.replace('\n', '') for x in w.readlines()]
+        self.v = v # Vowel.
+        self.c = c # Consonant.
+        self.decoded = [(x, pythorology.Vcipher(x).v.to_one(), pythorology.Vcipher(x).c.to_one()) for x in self.words]
+    
+    def _filter(self):
+    
+        return [x for x in self.decoded if x[1] == self.v and x[2] == self.c]
+        
 if __name__ == "__main__":
 
-	v = int(raw_input("Desired number for Vowels: "))
-	c = int(raw_input("Desired number for Consonants: "))
+    v = int(raw_input("Desired number for Vowels: "))
+    c = int(raw_input("Desired number for Consonants: "))
 
-	gen = WordGen(v, c)
-	print gen._filter()
+    gen = WordGen(v, c)
+    print gen._filter()
